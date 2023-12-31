92b6d832deb943e53692b90a0cc1aca7e44e0cd1
Dhruv9544 <dhruvshah905@gmail.com>
2023-12-31 19:47:00 +0530

    middleware added for wikipedia url

--- routes/CheckUrlRoute.js
+++ routes/CheckUrlRoute.js
@@ -1,8 +1,9 @@
 const checkUrlController = require('../controller/checkUrl')
+const matchSyntax = require('../middleware/matchSyantax')
 const express = require('express')
 
 const Router = express.Router()
 
-Router.post('/calculate',checkUrlController.CheckUrl)
+Router.post('/calculate',matchSyntax,checkUrlController.CheckUrl)
 
 module.exports = Router
